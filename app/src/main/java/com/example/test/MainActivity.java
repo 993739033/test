ae14b4f481889e3fa6e0af9652eb2ee2dad95f64
Scode <git@Scode.com>
2017-04-16 18:45:27 +0800

    second commit

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.button);
+        System.out.println("for test");
     }
 }
